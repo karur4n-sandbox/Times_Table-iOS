1d2e289bd1addc65bd20c84f6bd34b7fe3e17fa1
karur4n <karuran.f@gmail.com>
2015-10-04 22:23:13 +0900

    Add Times Table

--- Times Table/ViewController.swift
+++ Times Table/ViewController.swift
@@ -8,11 +8,32 @@
 
 import UIKit
 
-class ViewController: UIViewController {
+class ViewController: UIViewController, UITableViewDelegate, UITableViewDataSource {
 
+    @IBOutlet weak var sliderValue: UISlider!
+    @IBOutlet weak var table: UITableView!
+    
+    @IBAction func sliderMoved(sender: AnyObject) {
+        table.reloadData()
+    }
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
+    }
+    
+    func tableView(tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return 20
+    }
+    
+    func tableView(tableView: UITableView, cellForRowAtIndexPath indexPath: NSIndexPath) -> UITableViewCell {
+        let cell = UITableViewCell(style: .Default, reuseIdentifier: "Cell")
+        
+        let timesTable = Int(sliderValue.value * 20)
+        
+        cell.textLabel?.text = String(timesTable * (indexPath.row + 1))
+        
+        return cell
     }
 
     override func didReceiveMemoryWarning() {
